6d14d0c5cf26f926218e62f8785a370f4567d80f
papa0843 <37610900+papa0843@users.noreply.github.com>
2021-04-03 15:53:50 +0900

    Create ourfunc.cpp

---
+++ ourfunc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+void simon(int);
+
+int main()
+{
+	using namespace std;
+	simon(3);
+	cout << "정수를 하나 고르시오: "
+	int count;
+	cin >> count;
+	simon(count);
+	cout << "끝!" << endl;
+	return 0;
+}
+
+void simon (int n)
+{
+	using namespace std;
+	cout << "Simon 왈 발가락을 " << n << "번 두드려라." << endl;
+}
