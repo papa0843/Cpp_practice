07d6b534fe7b6c7e9d8ff649b892a0e3c8e3eec0
hamnahyun <papa0843@gmail.com>
2021-07-06 18:32:21 +0900

    Let's Start to Learn for loop

---
+++ 04_Loop/01_forloop.cpp
@@ -0,0 +1,13 @@
+// forloop.cpp -- introducing the for loop
+//
+#include <iostream>
+
+int main()
+{
+	int i;	//create a counter
+	// initialize; test; update
+	for ( i = 0; i < 5; i++)
+		std::cout << "C++ knows loops.\n";
+	std::cout << "C++ knows when to stop.\n";
+	return 0;
+}
